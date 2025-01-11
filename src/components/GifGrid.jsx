6d999153613b3606fb641c208f90298580662ee3
HardlinePizza21 <madridossa@gmail.com>
2025-01-10 21:34:50 -0500

    primer build para producción

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,30 +1,21 @@
+import { useState, useEffect } from "react";
+import { getGifs } from "../helpers/getGifs";
+
 export const GifGrid = ({category}) => {
 
+    const [counter, setCounter] = useState(10);
+    
+    useEffect( () => {
+        getGifs(category);
+    },[ ])
 
-    // const getGifs = async() => {
-
-    //     const url = `https://api.giphy.com/v1/gifs/search?api_key=WXLPnESSpDmBrt4fTgdQSkewoIN8jaHq&q=${category}&limit=10`
-    //     const resp = await fetch(url);
-    //     const {data} = await resp.json();
-        
-    //     console.log(data)
-
-    //     const gifs = data.map(img => ({
-    //         id: img.id,
-    //         title: img.title,
-    //         url: img.images.downsized_medium.url
-    //     }))
-
-    //     console.log(gifs)
-
-    // }
-
-    // getGifs();
 
     return (
         <>
             <h3>{category}</h3>
-            <p>Hola mundo</p>
+
+            <h5>{counter}</h5>
+            <button onClick={() => setCounter(counter + 1)}>+1</button>
         </>
     )
 }
